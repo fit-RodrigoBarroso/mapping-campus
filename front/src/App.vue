d275ca88e259cb68091c3ff75408ebacb41e6bf2
rodrigo <rodrigolb01@gmail.com>
2021-07-14 13:42:03 -0300

    Componente de Login

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,15 @@
 <template>
+    <Login></Login>
 </template>
 
 <script>
+import Login from "./components/Login.vue"
+    export default {
+        name: "App",
+        components: {
+            Login
+        }
+    }
 </script>
 
 <style>
---
+++ front/src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+<h1>
+    <div >
+        <form @submit="onSubmit">
+            <div>
+                <label for="">Login</label><br><input v-model="login" type="text">
+            </div>
+            <div>
+                <label for="">Senha</label><br><input v-model="password" type="text">
+            </div>
+            <button>login</button>
+            <button >Cadastrar</button>
+        </form>
+    </div>
+</h1>
+</template>
+
+<script>
+
+export default{
+    name: "Login",
+    data() {
+        return {
+            login: "",
+            password: ""
+        }
+    },
+    methods: {
+        onSubmit() {
+            console.log("login: ",this.login, "senha:", this.password)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
