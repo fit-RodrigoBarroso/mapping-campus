8402d8301eaf660ffd9210ad98c96f9e0570c85b
rodrigo <rodrigolb01@gmail.com>
2021-07-15 10:52:36 -0300

    style e componente login

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue';
 import router from '@/router';
 import App from './App.vue';
+import 'bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 createApp(App)
     .use(router)
